92bb1b07f43dd3acffc1d436e57a3e29676973aa
Zac Chang <zacchang1@gmail.com>
2019-08-23 14:32:46 +0800

    docs: redifine restart question
    Change the restart question to indicate the user
    type yes more possible instead of no.

--- bikeshare.py
+++ bikeshare.py
@@ -233,7 +233,7 @@ def main():
         user_stats(df)
         display_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nGive it another try? Yes could be a great answer to see more and a no will be fine.\n')
         if restart.lower() != 'yes':
             break
 
